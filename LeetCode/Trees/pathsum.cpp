e38905a237f65014193a9fee3dd5d1f263687a8f
Omanshu Mahawar <56030298+Omanshu840@users.noreply.github.com>
2020-07-25 23:35:44 +0530

    Trees

---
+++ LeetCode/Trees/pathsum.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        
+        if(root==NULL)
+        {
+            return false;
+        }
+        
+        if(root->left==NULL && root->right==NULL)
+        {
+            if(sum-root->val==0)
+                return true;
+        }
+        else if(root->left && root->right)
+        {
+            return hasPathSum(root->left, sum-root->val) || hasPathSum(root->right, sum-root->val);    
+        }   
+        else if(root->left==NULL)
+        {
+            return hasPathSum(root->right, sum-root->val);
+        }
+        else
+        {
+            return hasPathSum(root->left, sum-root->val);
+        }
+        
+        return false;
+        
+        
+        
+    }
+};
